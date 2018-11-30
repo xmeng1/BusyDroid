2f4fefaaa86ca90f5c97e81af5296f588adfb34f
Xin Meng <x.meng@outlook.com>
2018-11-30 05:45:15 +0000

    Basic textView Button and ImageView

--- feature/src/main/java/science/mengxin/android/busydroid/feature/MainActivity.kt
+++ feature/src/main/java/science/mengxin/android/busydroid/feature/MainActivity.kt
@@ -1,12 +1,18 @@
 package science.mengxin.android.busydroid.feature
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun someMethod(theButton: View) {
+        Log.v("Basic Study", "Button Click" + theButton.transitionName)
     }
 }
